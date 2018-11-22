cbd86920e71a1f56ba7b566811b565a0a8acd9a1
Soroosh Rezazadeh <soroosh.rezazadeh@gmail.com>
2018-11-22 17:08:59 -0500

    Basic map-reduce with mrjob

--- MapReduce/RatingsBreakdown.py
+++ MapReduce/RatingsBreakdown.py
@@ -3,10 +3,7 @@ from mrjob.step import MRStep
 
 class RatingsBreakdown(MRJob):
     def steps(self):
-        return [
-            MRStep(mapper=self.mapper_get_ratings,
-                   reducer=self.reducer_count_ratings)
-        ]
+        return [MRStep(mapper = self.mapper_get_ratings, reducer = self.reducer_count_ratings)]
 
     # Mapper function
     def mapper_get_ratings(self, _, line):
