466c698fdd2d7431ea87a5b91db9888a0dc17161
Soroosh Rezazadeh <soroosh.rezazadeh@gmail.com>
2019-03-05 11:18:50 -0500

    .

---
+++ MapReduce/TopMovies.py
@@ -0,0 +1,31 @@
+'''
+Sort the movies by their numbers of ratings
+
+By default, streaming treats all input and output as strings. So things get
+sorted as strings, not numerically
+'''
+
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class TopMovies(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_ratings, reducer=self.reducer_count_ratings),
+            MRStep(reducer=self.reducer_sorted_output)
+        ]
+
+    def mapper_get_ratings(self, _, line):
+        (userID, movieID, rating, timestamp) = line.split('\t')
+        yield movieID, 1   # key/value pairs
+
+    def reducer_count_ratings(self, key, values):
+        yield str(sum(values)).zfill(5), key  # (rating count, movieID)  and zero-pad our numbers
+
+    def reducer_sorted_output(self, count, movies):
+        for movie in movies:
+            yield movie, count
+
+
+if __name__ == '__main__':
+    TopMovies.run()
